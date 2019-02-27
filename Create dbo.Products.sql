10bfa0bb4d08b2805c5db7d2e99e5249ada3db7c
Mohit Dedhe <43933588+mdedhe@users.noreply.github.com>
2019-02-26 21:26:18 -0500

    Add files via upload

---
+++ Create dbo.Products.sql
@@ -0,0 +1,18 @@
+﻿USE [Product]
+GO
+
+/****** Object: Table [dbo].[Products] Script Date: 2/26/2019 9:22:30 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Products]
+(
+	[productid] INT NOT NULL PRIMARY KEY, 
+    [productName] VARCHAR(50) NULL, 
+    [departmentid] INT NULL FOREIGN KEY REFERENCES dbo.Sales(departmentid)
+)
+
+
---
+++ Create dbo.Sales.sql
@@ -0,0 +1,19 @@
+﻿USE [Product]
+GO
+
+/****** Object: Table [dbo].[Sales] Script Date: 2/26/2019 9:22:06 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Sales] (
+    [departmentid] INT          NOT NULL,
+    [Department]   VARCHAR (50) NULL,
+    [ProductName]  VARCHAR (50) NULL,
+    [sale_count]   VARCHAR (50) NULL,
+    [Product_id]   VARCHAR (50) NULL
+);
+
+
